0b089e9b2f40a581a4cd306b000bec4905dcf62e
coding-saints <coding.saints2015@gmail.com>
2020-07-05 14:43:01 -0400

    commited

--- site-config.js
+++ site-config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `Gatsby Universal`,
-  siteTitleShort: `GatsbyU`,
-  siteDescription: `An opinionated starter for Gatsby.`,
-  siteUrl: `https://gu.fabianschultz.com`,
+  siteTitle: `Take The Risk`,
+  siteTitleShort: `TakeTheRisk`,
+  siteDescription: `Life is too short for regrets.`,
+  siteUrl: `https://dreamy-lichterman-4e4a9d.netlify.app`,
   themeColor: `#000`,
   backgroundColor: `#fff`,
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
-    twitter: `gatsbyjs`,
-    fbAppId: `966242223397117`,
+    twitter: `nicholas_james0`
+    // fbAppId: `966242223397117`,
   },
 };
